1e6101404901e65dce1afd01b0ba521a65310a0f
david.theran03@gmail.com <david.theran03@gmail.com>
2025-11-20 21:49:30 -0500

    commit

--- src/shared/infra/http/OptimizedApiClient.ts
+++ src/shared/infra/http/OptimizedApiClient.ts
@@ -3,8 +3,8 @@ import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios';
 import type { HttpClientLike, RequestOptions } from '@/shared';
 
 
-const { VITE_API_BASE_URL } = import.meta.env;
-const BASE_URL = VITE_API_BASE_URL || 'http://localhost:8080';
+const { PUBLIC_API_BASE_URL_PROD } = import.meta.env;
+const BASE_URL = PUBLIC_API_BASE_URL_PROD || 'http://localhost:8080';
 
 export class OptimizedApiClient implements HttpClientLike {
   private readonly axiosInstance: AxiosInstance;
